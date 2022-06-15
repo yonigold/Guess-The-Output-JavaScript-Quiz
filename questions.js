b4c9a58a8d15b6ac7e4ee3d02bc35f92c8c7d4e0
yonigold <yoni7022@gmail.com>
2022-06-15 20:26:58 +0300

    Delete questions.js

--- questions.js
+++
@@ -1,7 +0,0 @@
-function ArrayBoolean(){
-  if([] && [1]) return [true, true];
-  else if([] && ![1]) return [true,false];
-  else if(![] && [1]) return [false,true];
-  else return [false,false];
-}
-ArrayBoolean();
